1f09bba4ba02dd8da3d4884caa9525d2873fe32f
Alexandr Elkin <mistodrake@gmail.com>
2022-10-12 03:48:04 +0600

    [FRONT] Navbar fixed

--- front/app/src/components/Navbar.vue
+++ front/app/src/components/Navbar.vue
@@ -1,28 +1,30 @@
 <template>
-  <nav class="blue-grey darken-3">
-    <div class="nav-wrapper">
-      <router-link to="/" class="brand-logo">Tasks</router-link>
-      <ul class="right hide-on-med-and-down">
-        <li>
-          <router-link
-              to="/"
-              exact
-              active-class="active"
-          >
-            <a href="#">Create</a>
-          </router-link>
-        </li>
-        <li>
-          <router-link
-              to="/list"
-              active-class="active"
-          >
-            <a href="#">List</a>
-          </router-link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  <div class="navbar-fixed">
+    <nav class="blue-grey darken-3">
+      <div class="nav-wrapper">
+        <router-link to="/" class="brand-logo">Tasks</router-link>
+        <ul class="right hide-on-med-and-down">
+          <li>
+            <router-link
+                to="/"
+                exact
+                active-class="active"
+            >
+              <a href="#">Create</a>
+            </router-link>
+          </li>
+          <li>
+            <router-link
+                to="/list"
+                active-class="active"
+            >
+              <a href="#">List</a>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
